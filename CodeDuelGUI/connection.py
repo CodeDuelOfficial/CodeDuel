9214ec56b94548e166998992361cf328c6e150e9
Venoox <63316015+VenooxDev@users.noreply.github.com>
2020-10-08 22:52:59 +0300

    main.py

---
+++ CodeDuelGUI/connection.py
@@ -0,0 +1,49 @@
+import json
+import time
+import socket
+
+
+class Connection:
+	"""Connection class for Client Connection"""
+	def __init__(self, host, port, encode_format ):
+		
+		self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+		self.host, self.port = host, port 
+		self.conn.connect((self.host, self.port))
+
+		self.encode_format = encode_format
+		self.HEADER = 128
+
+	def send(self, data):
+		self.conn.send(bytes(data, self.encode_format))
+
+	def recv(self, buffer):
+		return json.loads(self.conn.recv(buffer).encode(self.encode_format))
+
+	def start(self): # isim bulcam start olmasın ama ya
+
+		while self.state:
+			
+
+			send_data = yield  # Get data from outside of function
+
+			try:
+
+				self.send(send_data) # Short-cut for self.conn.send()
+
+				yield self.recv(self.HEADER)
+
+			except socket.error:
+
+				connected = False
+
+				while (not connected):
+
+					try:
+						self.conn.connect((self.host, self.port))
+					except:
+						time.sleep(2) 
+					else:
+						connected = True
+
--- CodeDuelGUI/main.py
+++ CodeDuelGUI/main.py
@@ -1,11 +1,16 @@
 from homescreenn import*
 from loginregister import*
-
+from connection import*
 
 class Main():
     def __init__(self):
         self.loginregister = LoginForm(app)
         self.loginregister.show()
+
+    def connect(self):
+        self.conn = Connection(socket.gethostname(), 8080 , 'utf-8')
+        self.connloop = self.conn.start()
+        self.connloop.send()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

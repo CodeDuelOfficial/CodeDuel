41e119769609fdb4305a1509e058c182b66fd914
Pyro Soft <47942890+asanadam12@users.noreply.github.com>
2020-10-09 23:04:11 +0300

    Update main.py

--- CodeDuelGUI/main.py
+++ CodeDuelGUI/main.py
@@ -6,32 +6,31 @@ from homescreenn import*
 from loginregister import*
 from connection import*
 
+@LoginForm.command(name = "login")
+def login(username,email,password):
+    pass
+
 class Main():
-    def __init__(self):
-        # Send Connecion request to the server
-        self.connect()
+    def __init__(self, conn):
+        # Initialize variables
+        self.conn = conn
         # Initialize GUI Forms
         self.loginregister = LoginForm()
-
         self.homescreen = HomeScreen()
         
     def start(self):
         self.loginregister.show()
-        
         self.homescreen.show()
         
-    def connect(self):
-        pass
     
-    @LoginForm.command(name = "login")
-    def login(self,username,email,password):
-        pass
+
     
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
-    main = Main()
+        
+    conn = Connection().start() # Send Connecion request to the server
+    main = Main(conn)
     main.start()
     
     sys.exit(app.exec_())

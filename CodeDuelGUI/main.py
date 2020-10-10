7aac27b8ab91facee19a47cc8f482f16d8243dc4
Pyro Soft <47942890+asanadam12@users.noreply.github.com>
2020-10-10 21:10:02 +0300

    Update main.py

--- CodeDuelGUI/main.py
+++ CodeDuelGUI/main.py
@@ -3,13 +3,18 @@
 @author:  Efe Osman ASLANOĞLU (PyroSoft) ,  Emircan DEMİRCİ(Venoox)
 @date: 8.10.2020
 """
+
 from homescreenn import*
 from loginregister import*
 from connection import*
 
 @LoginForm.command(name = "login")
-def login(username,email,password):
-    pass
+def login(email,password):
+    return conn.send({"email": email, "password": password})
+
+@LoginForm.command(name = "register")
+def register(username,email,password):
+    return conn.send({"username": username, "email": email, "password": password})
 
 class Main():
     def __init__(self, conn):
@@ -29,9 +34,10 @@ class Main():
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-        
+    
     conn = Connection().start() # Send Connecion request to the server
+    next(conn) # start generator for yield assigment.
+    
     main = Main(conn)
     main.start()
-    
     sys.exit(app.exec_())

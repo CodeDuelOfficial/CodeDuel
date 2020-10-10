9c71e4fdd14940628a14262c08e0957b120b33a3
Venoox <63316015+VenooxDev@users.noreply.github.com>
2020-10-10 21:39:04 +0300

    asd

--- CodeDuelGUI/loginregister.py
+++ CodeDuelGUI/loginregister.py
@@ -12,42 +12,48 @@ from PyQt5.QtCore import QCoreApplication, Qt,QBasicTimer, QPoint
 import sys
 
 class LoginForm(QWidget):
-    commands = {"login": None, "register": None}
+    binds = {"login": None, "register": None}
     def __init__(self):
         super().__init__()
 
-        self.mwidget = QMainWindow(self)
+        self.mainwidget = QMainWindow(self)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 
-        self.stacked_widget3 = QStackedWidget(self)
-        self.stacked_widget3.setGeometry(0,70,600,400)
+        self.main_stackedWidget = QStackedWidget(self)
+        self.main_stackedWidget.setGeometry(0,70,600,400)
 
         self.login_page = QWidget()
-        self.stacked_widget3.addWidget(self.login_page)
+        self.main_stackedWidget.addWidget(self.login_page)
 
         self.register_page = QWidget()
-        self.stacked_widget3.addWidget(self.register_page)
+        self.main_stackedWidget.addWidget(self.register_page)
 
-        #size
+        #main screen properties
         self.setFixedSize(600,400)
-        self.center()
-        self.logButtonClicked()
         self.setStyleSheet("background-color:rgb(35,35,35);")
 
+        #functions
+        self.center()
+        self.login_screen()
 
-        #label1
-        self.label1 = QLabel("Code" , self)
-        self.label1.setFont(QFont('Firestarter' , 30))
-        self.label1.setGeometry(205,0,150,60)
-        self.label1.setAlignment(Qt.AlignTop)
-        self.label1.setStyleSheet("color:red")
+        #main_code_label
+        self.main_code_label = QLabel("Code" , self)
+        self.main_code_label.setFont(QFont('Arial' , 30))
+        self.main_code_label.setGeometry(205,0,150,60)
+        self.main_code_label.setAlignment(Qt.AlignTop)
+        self.main_code_label.setStyleSheet("color:red")
 
-        #duelLabel
-        self.duelLabel = QLabel("Duel" , self)
-        self.duelLabel.setFont(QFont('Firestarter' , 30))
-        self.duelLabel.setGeometry(300,0,150,60)
-        self.duelLabel.setAlignment(Qt.AlignTop)
-        self.duelLabel.setStyleSheet("color:orange")
+        #main_duel_label
+        self.main_duel_label = QLabel("Duel" , self)
+        self.main_duel_label.setFont(QFont('Firestarter' , 30))
+        self.main_duel_label.setGeometry(300,0,150,60)
+        self.main_duel_label.setAlignment(Qt.AlignTop)
+        self.main_duel_label.setStyleSheet("color:orange")
+
+        #######################################
+        ##titlebar variables
+        #######################################
+
 
         #closebox
         self.closebox = QPushButton("X" , self)
@@ -65,15 +71,7 @@ class LoginForm(QWidget):
 
         self.oldPos = self.pos()
 
-
-    @classmethod
-    def command(cls,name):
-        assert (name in cls.commands)
-
-        def getFunc(func):
-            cls.commands[name] = func
-        return getFunc
-    #center
+    #titlebar movement functions
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
@@ -97,152 +95,163 @@ class LoginForm(QWidget):
     def minimizeboxEvent(self):
     	self.showMinimized()
 
+    
+    #####################################
+    ##server-side
+    ####################################
+    @classmethod
+    def command(cls,name):
+        assert (name in cls.binds)
 
+        def getFunc(func):
+            cls.binds[name] = func
+        return getFunc   
 
-        
-    #letsgobuttondb
-    def letsgoDB(self):
-        if self.login_email_textbox.text() == "" or self.passtextbox.text() == "":
-            letsgoerror = QMessageBox()
-            letsgoerror.setStyleSheet("background-color:dimgray;color:silver;")
-            letsgoerror.setText("Please fill in all blanks")
-            letsgoerror.setWindowTitle("Error")
-            letsgoerror.setIcon(QMessageBox.Warning)
-            letsgoerror.setWindowIcon(QIcon("Buttons/warning.png"))
-            letsgoerror.exec_()
+    #Login Database
+    def loginDatabaseConn(self):
+        if self.login_email_textbox.text() == "" or self.login_pass_textbox.text() == "":
+            login_error_messagebox = QMessageBox()
+            login_error_messagebox.setStyleSheet("background-color:dimgray;color:silver;")
+            login_error_messagebox.setText("Please fill in all blanks")
+            login_error_messagebox.setWindowTitle("Error")
+            login_error_messagebox.setIcon(QMessageBox.Warning)
+            login_error_messagebox.setWindowIcon(QIcon("Buttons/warning.png"))
+            login_error_messagebox.exec_()
         else:
-            email , passwd = self.login_email_textbox.text() , self.passtextbox.text()
+            email , passwd = self.login_email_textbox.text() , self.login_pass_textbox.text()
             command_result = self.binds["login"](email, passwd)
     
     #registerlabel
     def registerLabelClicked(self):
-        #emaillabel
-        self.emaillabel = QLabel("Email" , self.register_page)
-        self.emaillabel.setGeometry(100,5,100,100)
-        self.emaillabel.setFont(QFont('Arial',18))
-        self.emaillabel.setStyleSheet('color:silver')
-        self.emaillabel.setAlignment(Qt.AlignTop)
+        #register_email_label
+        self.register_email_label = QLabel("Email" , self.register_page)
+        self.register_email_label.setGeometry(100,5,100,100)
+        self.register_email_label.setFont(QFont('Arial',18))
+        self.register_email_label.setStyleSheet('color:silver')
+        self.register_email_label.setAlignment(Qt.AlignTop)
 
-        #emailtextbox
-        self.emailtext = QLineEdit(self.register_page)
-        self.emailtext.move(200,0)
-        self.emailtext.resize(300,30)
-        self.emailtext.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
+        #register_email_textbox
+        self.register_email_text = QLineEdit(self.register_page)
+        self.register_email_text.move(200,0)
+        self.register_email_text.resize(300,30)
+        self.register_email_text.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
 
         #uslabel
-        self.username = QLabel("Username",self.register_page)
-        self.username.setFont(QFont('Arial' , 18))
-        self.username.setGeometry(55,50,120,85)
-        self.username.setStyleSheet("color:#BFBFBF;")
+        self.register_username_label = QLabel("Username",self.register_page)
+        self.register_username_label.setFont(QFont('Arial' , 18))
+        self.register_username_label.setGeometry(55,50,120,85)
+        self.register_username_label.setStyleSheet("color:#BFBFBF;")
 
-        #ustextbox
-        self.ustextbox = QLineEdit(self.register_page)
-        self.ustextbox.move(200,75)
-        self.ustextbox.resize(300,30)
-        self.ustextbox.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
+        #register_username_textbox
+        self.register_username_textbox = QLineEdit(self.register_page)
+        self.register_username_textbox.move(200,75)
+        self.register_username_textbox.resize(300,30)
+        self.register_username_textbox.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
 
-        #passlabel
-        self.passlabel = QLabel("Password",self.register_page)
-        self.passlabel.setFont(QFont('Arial' , 18))
-        self.passlabel.setGeometry(55,125,120,85)
-        self.passlabel.setStyleSheet("color:#BFBFBF;")
+        #register_password_label
+        self.register_password_label = QLabel("Password",self.register_page)
+        self.register_password_label.setFont(QFont('Arial' , 18))
+        self.register_password_label.setGeometry(55,125,120,85)
+        self.register_password_label.setStyleSheet("color:#BFBFBF;")
 
 
-        #passtextbox
-        self.passtextbox1 = QLineEdit(self.register_page)
-        self.passtextbox1.move(200,150)
-        self.passtextbox1.resize(300,30)
-        self.passtextbox1.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
-        self.passtextbox1.setEchoMode(QLineEdit.Password)
+        #login_pass_textbox
+        self.register_pass_textbox = QLineEdit(self.register_page)
+        self.register_pass_textbox.move(200,150)
+        self.register_pass_textbox.resize(300,30)
+        self.register_pass_textbox.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
+        self.register_pass_textbox.setEchoMode(QLineEdit.Password)
+
+        #confirm text box label
         self.confirm_label = QLabel("Confirm" , self.register_page)
         self.confirm_label.setFont(QFont('Arial' , 18))
         self.confirm_label.setGeometry(70,200,120,70)
         self.confirm_label.setStyleSheet("color:#BFBFBF")
 
-        #passtextbox
-        self.confirm_textbox = QLineEdit(self.register_page)
-        self.confirm_textbox.move(200,218)
-        self.confirm_textbox.resize(300,30)
-        self.confirm_textbox.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
-        self.confirm_textbox.setEchoMode(QLineEdit.Password)
+        #register password textbox
+        self.register_confirm_pass_textbox = QLineEdit(self.register_page)
+        self.register_confirm_pass_textbox.move(200,218)
+        self.register_confirm_pass_textbox.resize(300,30)
+        self.register_confirm_pass_textbox.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
+        self.register_confirm_pass_textbox.setEchoMode(QLineEdit.Password)
 
-        #registerbutton
+        #register button add variables to database
         self.registerbutton = QPushButton("Register" , self.register_page)
         self.registerbutton.setGeometry(250,265,100,30)
         self.registerbutton.setStyleSheet("background-color:silver;border-style:outset;border-width:2px;border-radius:15px;padding:6px;border-color:#264348;")
         self.registerbutton.clicked.connect(self.registerDB)
 
-        #logbutton
-        self.logbutton = QPushButton("L͟o͟g͟i͟n͟",self.register_page)
-        self.logbutton.setGeometry(270,305,50,20)
-        self.logbutton.setFlat(True)
-        self.logbutton.clicked.connect(self.logButtonClicked)
-        self.logbutton.setFont(QFont('Arial' , 8))
-        self.logbutton.setStyleSheet("background-color:gray;color:rgb(175,93,72)")
+        #login linked button
+        self.go_login_screen = QPushButton("L͟o͟g͟i͟n͟",self.register_page)
+        self.go_login_screen.setGeometry(270,305,50,20)
+        self.go_login_screen.setFlat(True)
+        self.go_login_screen.clicked.connect(self.login_screen)
+        self.go_login_screen.setFont(QFont('Arial' , 8))
+        self.go_login_screen.setStyleSheet("background-color:gray;color:rgb(175,93,72)")
 
 
-        self.stacked_widget3.setCurrentIndex(1)
+        self.main_stackedWidget.setCurrentIndex(1)#go to the register page
     
     def registerDB(self):
-        if self.emailtext.text() == "" or self.passtextbox1.text() == "" or self.confirm_textbox.text() == "" or self.ustextbox.text() == "":
-            registererror = QMessageBox()
-            registererror.setStyleSheet("background-color:dimgray;color:silver;")
-            registererror.setText("Please fill in all blanks")
-            registererror.setWindowTitle("Error")
-            registererror.setIcon(QMessageBox.Warning)
-            registererror.setWindowIcon(QIcon("Buttons/warning.png"))
-            registererror.exec_()
-        elif self.passtextbox1.text() != self.confirm_textbox.text():
-            passworderror = QMessageBox()
-            passworderror.setStyleSheet("background-color:dimgray;color:silver;")
-            passworderror.setText("Passwords do not match")
-            passworderror.setWindowTitle("Error")
-            passworderror.setIcon(QMessageBox.Warning)
-            passworderror.setWindowIcon(QIcon("Buttons/warning.png"))
-            passworderror.exec_()
+        if self.register_email_text.text() == "" or self.register_pass_textbox.text() == "" or self.confirm_textbox.text() == "" or self.register_username_textbox.text() == "":
+            register_error_messagebox = QMessageBox()
+            register_error_messagebox.setStyleSheet("background-color:dimgray;color:silver;")
+            register_error_messagebox.setText("Please fill in all blanks")
+            register_error_messagebox.setWindowTitle("Error")
+            register_error_messagebox.setIcon(QMessageBox.Warning)
+            register_error_messagebox.setWindowIcon(QIcon("Buttons/warning.png"))
+            register_error_messagebox.exec_()
+        elif self.login_pass_textbox1.text() != self.confirm_textbox.text():
+            register_pass_error_messagebox = QMessageBox()
+            register_pass_error_messagebox.setStyleSheet("background-color:dimgray;color:silver;")
+            register_pass_error_messagebox.setText("Passwords do not match")
+            register_pass_error_messagebox.setWindowTitle("Error")
+            register_pass_error_messagebox.setIcon(QMessageBox.Warning)
+            register_pass_error_messagebox.setWindowIcon(QIcon("Buttons/warning.png"))
+            register_pass_error_messagebox.exec_()
         else:
-            username, email, passwd = self.ustextbox.text() , self.emailtext.text() , self.passtextbox1.text()
-            command_result = self.binds["register"](username, email, passwd)
-    def logButtonClicked(self):
-        #usernamelabel
-        self.label2 = QLabel("Email" , self.login_page)
-        self.label2.setFont(QFont('Arial' , 20))
-        self.label2.setGeometry(100,20,100,40)
-        self.label2.setStyleSheet("color:#BFBFBF;")
+            register_username_label, email, passwd = self.register_username_textbox.text() , self.register_email_text.text() , self.register_pass_textbox.text()
+            command_result = self.binds["register"](register_username_label, email, passwd)
+    def login_screen(self):
+        #login email label
+        self.login_email_label = QLabel("Email" , self.login_page)
+        self.login_email_label.setFont(QFont('Arial' , 20))
+        self.login_email_label.setGeometry(100,20,100,40)
+        self.login_email_label.setStyleSheet("color:#BFBFBF;")
 
     
-        # usernametextbox
+        # login email textbox
         self.login_email_textbox = QLineEdit(self.login_page)
         self.login_email_textbox.move(250,22)
         self.login_email_textbox.resize(300,30)
         self.login_email_textbox.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
 
-        #passtextbox
-        self.passtextbox = QLineEdit(self.login_page)
-        self.passtextbox.move(250,130)
-        self.passtextbox.resize(300,30)
-        self.passtextbox.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
-        self.passtextbox.setEchoMode(QLineEdit.Password)
-        #passlabel
-        self.label3 = QLabel("Password",self.login_page)
-        self.label3.setFont(QFont('Arial' , 18))
-        self.label3.setGeometry(100,86,120,120)
-        self.label3.setStyleSheet("color:#BFBFBF;")
+        #login_pass_textbox
+        self.login_pass_textbox = QLineEdit(self.login_page)
+        self.login_pass_textbox.move(250,130)
+        self.login_pass_textbox.resize(300,30)
+        self.login_pass_textbox.setStyleSheet("border: 3px solid dimgray;border-style:outset;border-width:2px;border-radius:10px;color:silver")
+        self.login_pass_textbox.setEchoMode(QLineEdit.Password)
+        #login passwor label
+        self.login_password_label = QLabel("Password",self.login_page)
+        self.login_password_label.setFont(QFont('Arial' , 18))
+        self.login_password_label.setGeometry(100,86,120,120)
+        self.login_password_label.setStyleSheet("color:#BFBFBF;")
 
-        #letsgobutton
+        #letsgo(login) button check informations
         self.letsgobutton = QPushButton("Lets Go" , self.login_page)
         self.letsgobutton.setGeometry(250,200,100,30)
         self.letsgobutton.setStyleSheet("background-color:silver;border-style:outset;border-width:2px;border-radius:15px;padding:6px;border-color:#264348;")
-        self.letsgobutton.clicked.connect(self.letsgoDB)
+        self.letsgobutton.clicked.connect(self.loginDatabaseConn)
 
 
-        #registerlinklabel
-        self.regButton = QPushButton("R͟e͟g͟i͟s͟t͟e͟r͟",self.login_page)
-        self.regButton.setGeometry(270,255,50,20)
-        self.regButton.clicked.connect(self.registerLabelClicked)
-        self.regButton.setFlat(True)
-        self.regButton.setFont(QFont('Arial' , 8))
-        self.regButton.setStyleSheet("background-color:rgb(35,35,35);color:rgb(175,93,72)")
-        self.stacked_widget3.setCurrentIndex(0)
+        #register linked label
+        self.register_linked_label = QPushButton("R͟e͟g͟i͟s͟t͟e͟r͟",self.login_page)
+        self.register_linked_label.setGeometry(270,255,50,20)
+        self.register_linked_label.clicked.connect(self.registerLabelClicked)
+        self.register_linked_label.setFlat(True)
+        self.register_linked_label.setFont(QFont('Arial' , 8))
+        self.register_linked_label.setStyleSheet("background-color:rgb(35,35,35);color:rgb(175,93,72)")
+
+        self.main_stackedWidget.setCurrentIndex(0)#go to the login page
     
-
